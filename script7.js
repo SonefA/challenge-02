25fb429ff7d92eff9285ab00f365c9a6fc543df9
SonefA <96904866+SonefA@users.noreply.github.com>
2022-03-12 14:49:25 +0700

    Update script7.js

--- script7.js
+++ script7.js
@@ -1,36 +1,38 @@
-const dataPenjualanPakAldi = [
+function hitungTotalPenjualan(dataPenjualan) 
+{
+    let totalSepatu = 0;
+    dataPenjualanPakAldi.map((item) => 
     {
-      namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
-      hargaSatuan: 760000,
-      kategori : "Sepatu Sport",
-      totalTerjual : 90,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Tristan Black Brown High',
-      hargaSatuan: 960000,
-      kategori : "Sepatu Sneaker",
-      totalTerjual : 37,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Tristan Maroon High ',
-      kategori : "Sepatu Sneaker",
-      hargaSatuan: 360000,
-      totalTerjual : 90,
-    },
-    {
-      namaProduct : 'Sepatu Warrior Rainbow Tosca Corduroy',
-      hargaSatuan: 120000,
-      kategori : "Sepatu Sneaker",
-      totalTerjual : 90,
-    }
-  ]
-
-  function hitungTotalPenjualan(dataPenjualan) {
-    let totalSepatu =0;
-    dataPenjualanPakAldi.map((item) => {
-        totalSepatu +=item.totalTerjual;
+    totalSepatu +=item.totalTerjual;
     })
     return totalSepatu;
-  }
+}
+
+const dataPenjualanPakAldi = [
+{
+    namaProduct : 'Sepatu Futsal Nike Vapor Academy 8',
+    hargaSatuan: 760000,
+    kategori : "Sepatu Sport",
+    totalTerjual : 90,
+},
+{
+    namaProduct : 'Sepatu Warrior Tristan Black Brown High',
+    hargaSatuan: 960000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 37,
+},
+{
+    namaProduct : 'Sepatu Warrior Tristan Maroon High ',
+    kategori : "Sepatu Sneaker",
+    hargaSatuan: 360000,
+    totalTerjual : 90,
+},
+{
+    namaProduct : 'Sepatu Warrior Rainbow Tosca Corduroy',
+    hargaSatuan: 120000,
+    kategori : "Sepatu Sneaker",
+    totalTerjual : 90,
+}
+]
 
   console.log(hitungTotalPenjualan(dataPenjualanPakAldi))
